0a714491578b82fc60a69bc689efc5bcd43abe75
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-03-31 12:04:35 +0530

    added white and black blocks to the board

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,5 +1,5 @@
 var colInDiv = $('#colInDiv');
-
+var boardBlocksArr = [];
 $(document).ready(function() {
     let widthParentDiv = colInDiv.width();
     let heightParentDiv = colInDiv.height();
@@ -19,6 +19,7 @@ $(document).ready(function() {
                 let div = document.createElement('div');
                 $(div).css('width', widthOfOneDiv);
                 $(div).css('height', heightOfOneDiv);
+                $(div).css('position', 'relative');
                 if (j % 2 == 0) {
                     $(div).css('backgroundColor', 'white');
                     $(row).append(div);
@@ -34,6 +35,7 @@ $(document).ready(function() {
                 let div = document.createElement('div');
                 $(div).css('width', widthOfOneDiv);
                 $(div).css('height', heightOfOneDiv);
+                $(div).css('position', 'relative');
                 if (j % 2 == 0) {
                     $(div).css('backgroundColor', 'black');
                     $(row).append(div);
@@ -44,5 +46,93 @@ $(document).ready(function() {
             }
         }
         colInDiv.append(row);
+        boardBlocksArr.push(row);
     }
+    createWhiteBlocks();
+    createBlackBlocks();
+    setWhiteBlocksToTheirOwnBlocks();
+    setBlackBlocksToTheirOwnBlocks();
+    setDroppableFunctionToCharacterBlocks();
 })
+var whiteBlocks = [];
+var blackBlocks = [];
+var whiteScoreBoard = $('#whitesScoreBoard');
+var blackScoreBoard = $('#blacksScoreBoard');
+var firstDivInWhiteScoreBoard = $(whiteScoreBoard).children('div:nth-child(1)').children('.secondRow').children('div:nth-child(1)');
+
+function createWhiteBlocks() {
+    for (let i = 0; i < 16; i++) {
+        let div = document.createElement('div');
+        $(div).css('width', 50);
+        $(div).css('height', 50);
+        $(div).css('backgroundColor', "#E1DFDB");
+        $(div).css('border-radius', 50);
+        $(div).css('position', 'absolute');
+        whiteBlocks.push(div);
+        $(whiteBlocks[i]).css('position', 'absolute');
+        $(whiteBlocks[i]).css('inset', '0 0 0 0');
+        $(whiteBlocks[i]).css('margin', 'auto');
+    }
+}
+
+function createBlackBlocks() {
+    for (let i = 0; i < 16; i++) {
+        let div = document.createElement('div');
+        $(div).css('width', 50);
+        $(div).css('height', 50);
+        $(div).css('backgroundColor', "#87847E");
+        $(div).css('border-radius', 50);
+        $(div).css('position', 'absolute');
+        blackBlocks.push(div);
+        $(blackBlocks[i]).css('position', 'absolute');
+        $(blackBlocks[i]).css('inset', '0 0 0 0');
+        $(blackBlocks[i]).css('margin', 'auto');
+    }
+}
+
+function setWhiteBlocksToTheirOwnBlocks() {
+    for (let i = 0; i < 2; i++) {
+        if (i == 0) {
+            for (let j = 0; j < 8; j++) {
+                $(boardBlocksArr[i]).children('div').eq(j).append(whiteBlocks[j]);
+            }
+        } else {
+            let index = 0;
+            for (let j = 8; j < 16; j++) {
+                $(boardBlocksArr[i]).children('div').eq(index).append(whiteBlocks[j]);
+                index++;
+            }
+        }
+    }
+}
+
+function setBlackBlocksToTheirOwnBlocks() {
+    for (let i = 6; i < 8; i++) {
+        if (i == 6) {
+            for (let j = 0; j < 8; j++) {
+                $(boardBlocksArr[i]).children('div').eq(j).append(blackBlocks[j]);
+            }
+        } else {
+            let index = 0;
+            for (let j = 8; j < 16; j++) {
+                $(boardBlocksArr[i]).children('div').eq(index).append(blackBlocks[j]);
+                index++;
+            }
+        }
+    }
+}
+
+function setDroppableFunctionToCharacterBlocks() {
+    for (let i = 0; i < whiteBlocks.length; i++) {
+        $(whiteBlocks[i]).draggable();
+        $(blackBlocks[i]).draggable();
+    }
+
+    for (let j = 0; j < boardBlocksArr.length; j++) {
+        $(boardBlocksArr[j]).droppable({
+            drop: function(event, ui) {
+                console.log('dropped')
+            }
+        })
+    }
+}
